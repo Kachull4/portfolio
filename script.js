eb7a91770c531ce4222421d2202ebbc3cfcfc6f0
Kateřina Beranová <katerina.beranova4@gmail.com>
2021-08-23 13:45:16 +0200

    experience

--- script.js
+++ script.js
@@ -4,34 +4,6 @@ var scroll =
     window.setTimeout(callback, 1000 / 60);
   };
 var elementsToShow = document.querySelectorAll(".show-on-scroll");
-
-function loop() {
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add("is-visible");
-    } else {
-      element.classList.remove("is-visible");
-    }
-  });
-  scroll(loop);
-}
-loop();
-function isElementInViewport(el) {
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0 && rect.bottom >= 0) ||
-    (rect.bottom >=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <=
-        (window.innerHeight || document.documentElement.clientHeight)) ||
-    (rect.top >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
   slidesPerView: 8,
@@ -50,11 +22,11 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-var menuLinks = document.querySelectorAll(".main-menu__link") 
-menuLinks.forEach(function(menuLink) {
-  menuLink.addEventListener("click",function(event){
-    event.preventDefault()
-   var linkHref = menuLink.getAttribute("href")
-  document.querySelector(linkHref).scrollIntoView({behavior:"smooth"})
-  })
-})
+var menuLinks = document.querySelectorAll(".main-menu__link");
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    var linkHref = menuLink.getAttribute("href");
+    document.querySelector(linkHref).scrollIntoView({ behavior: "smooth" });
+  });
+});
