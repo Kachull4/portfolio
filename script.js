116f0f5bcbb0aecd4c07f74bfb5411fae4a7e715
Kachull4 <katerina.beranova4@gmail.com>
2026-07-22 09:35:08 +0200

    clean up

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-var scroll =
-  window.requestAnimationFrame ||
-  function (callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-var elementsToShow = document.querySelectorAll(".show-on-scroll");
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 10,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
-
-var menuLinks = document.querySelectorAll(".main-menu__link");
-menuLinks.forEach(function (menuLink) {
-  menuLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    var linkHref = menuLink.getAttribute("href");
-    document.querySelector(linkHref).scrollIntoView({ behavior: "smooth" });
+if (
+  typeof Swiper !== "undefined" &&
+  document.querySelector(".mySwiper") &&
+  document.querySelector(".mySwiper2")
+) {
+  const certificateThumbnails = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 10,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
   });
-});
+
+  new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: certificateThumbnails,
+    },
+  });
+}
